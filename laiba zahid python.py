816fa02bc8939b1321535c07bc4edb680f4d2304
LaibaZahid23 <laibazahid.643@gmail.com>
2024-11-02 18:26:52 +0500

    Add files via upload

---
+++ laiba zahid python.py
@@ -0,0 +1,35 @@
+number1=20
+number2=30
+number3=340
+#FINDING sum
+sum=number1+number2+number3
+print("sum:",sum)
+#FINDING difference
+difference=number1+number2+number3
+print("difference:",difference)
+#FINDING product
+product=number1+number2+number3
+print("product:",product)
+#FINDING average
+average=(number1+number2+number3)
+print("average:",average)
+
+#area of triangle
+breadth=6
+height=7
+area=0.5*breadth*height
+print("triangle area",area)
+#area of square
+side=5
+area=side*side
+print("square area",area)
+#area of circle
+pi=3.20
+radius=5
+area=pi*radius*radius
+print("circle area",area)
+#area of reactangle
+length=30
+width=10
+area=length*width
+print("rectangle area",area)
